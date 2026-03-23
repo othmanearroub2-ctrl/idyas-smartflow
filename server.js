a22da05411105304fe72e686b1e8a4fd5d06c30f
othmanearroub2-ctrl <othmanearroub2@gmail.com>
2026-03-23 17:58:20 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.use(cors({
 app.use(express.json());
 
 // --- MongoDB Connexion ---
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('✅ Connecté à MongoDB Atlas'))
 .catch(err => console.error('❌ Erreur de connexion MongoDB :', err));
 
